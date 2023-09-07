48412c611c9a006a186a04275043c7e65f3c31f6
Yanjilong <yanjl@allinpay.com>
2023-09-07 14:44:56 +0800

    login2

--- backEnd/superlion/controller/testController.go
+++ backEnd/superlion/controller/testController.go
@@ -6,23 +6,32 @@ import (
 	"superlion/service"
 )
 
+/**
+首页测试
+*/
 func Index(c *gin.Context) {
 
 	/* 处理请求 */
 	c.JSONP(200, gin.H{"msg": "Hello index 2!"})
 }
 
+/**
+测试接口
+*/
 func Hello(c *gin.Context) {
 
 	/* 处理请求 */
 	c.JSONP(200, gin.H{"msg": "Hello World 2!"})
 }
 
+/**
+登录测试
+*/
 func Login(c *gin.Context) {
 
 	req := bean.LoginReq{
-		c.Param("name"),
-		c.Param("passwd"),
+		c.PostForm("name"),
+		c.PostForm("passwd"),
 	}
 	/* 处理请求 */
 	data, err := service.Login(&req)
