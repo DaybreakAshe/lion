0d18231c914b8091758f9ae144f56c1f6243e7ce
Yanjilong <yanjl@allinpay.com>
2023-09-11 22:23:47 +0800

    跨域

--- backEnd/superlion/main.go
+++ backEnd/superlion/main.go
@@ -42,7 +42,7 @@ func cors() gin.HandlerFunc {
 		// 接收所有
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
-		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		c.Header("Access-Control-Allow-Headers", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
 		if c.Request.Method == "OPTIONS" {
