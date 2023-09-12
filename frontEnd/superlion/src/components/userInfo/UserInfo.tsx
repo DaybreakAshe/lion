4c349e71cd2b5d61cd35162302b34cb763d7a86d
HuangKaiFeng <73734562+DaybreakAshe@users.noreply.github.com>
2023-09-12 09:47:36 +0800

    fix google url

--- frontEnd/superlion/src/components/userInfo/UserInfo.tsx
+++ frontEnd/superlion/src/components/userInfo/UserInfo.tsx
@@ -146,7 +146,7 @@ const UserInfo = () => {
     const redirectUri = pathSegments === 'http://localhost:3000' ? 'http://localhost:3000' : 'https://superlion.vercel.app'
     const youtubeLogin = async () => {
         setLoading(true)
-        window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&include_granted_scopes=true&response_type=token&state=3EAB37D9D5310BFE&redirect_uri=${redirectUri}&client_id=32041706814-n36purujenfckur3831hkjgipbc4plia.apps.googleusercontent.com`
+        window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&include_granted_scopes=true&response_type=token&state=3EAB37D9D5310BFE&redirect_uri=${redirectUri}&client_id=32041706814-n36purujenfckur3831hkjgipbc4plia.apps.googleusercontent.com`
     }
     const parseUrl = (url: string) => {
         if (url.indexOf("#") > -1) {
