0f5cd865382c41f2a6254f9a879f5539894ef667
HuangKaiFeng <73734562+DaybreakAshe@users.noreply.github.com>
2023-09-20 15:52:18 +0800

    add  file upload

--- frontEnd/superlion/src/services/createBlog/createBlog.service.ts
+++ frontEnd/superlion/src/services/createBlog/createBlog.service.ts
@@ -4,9 +4,14 @@ interface fileProps {
     picture: any,
     busiType: string,
 }
+
 export const uploadFile = async (prop: fileProps) => {
     try {
-        const response = await axiosApi().post('/auth/upload', prop)
+        const response = await axiosApi().post('/auth/upload', prop, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
         return response.data
     } catch (error: any) {
         return error.response;
