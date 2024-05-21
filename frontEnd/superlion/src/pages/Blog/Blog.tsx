3847f87669a791320b6321978a6ad45518e20d96
HuangKaiFeng <73734562+DaybreakAshe@users.noreply.github.com>
2024-05-21 14:52:15 +0800

    style

--- frontEnd/superlion/src/pages/Blog/Blog.tsx
+++ frontEnd/superlion/src/pages/Blog/Blog.tsx
@@ -1,50 +1,87 @@
 import { Box } from "@mui/material";
-import { makeStyles } from '@mui/styles'
-import BlogCard from "./BlogCard"
-import { BlogProps } from "src/models/blog"
-const useStyles = makeStyles(() => ({
-    root: {
-        
-    },
-}))
+import BlogCard from "./BlogCard";
+import { BlogProps } from "src/models/blog";
 
 const Blog = () => {
-    const classes = useStyles()
-    const listData: BlogProps[] = [
-        {
-            id:"111",
-            title:"title1",
-            content:"content1",
-            time:111
-        },
-        {
-            id: "222",
-            title:"title2",
-            content:"content2",
-            time:222
-        },
-        {
-            id:"333",
-            title:"title3",
-            content:"content3",
-            time:333
-        }
-    ]
+  const listData: BlogProps[] = [
+    {
+      id: "111",
+      title: "是楷体吗",
+      content: "content1",
+      time: 111,
+    },
+    {
+      id: "222",
+      title: "title2",
+      content: "content2",
+      time: 222,
+    },
+    {
+      id: "333",
+      title: "title3",
+      content: "content3",
+      time: 333,
+    },
+    {
+      id: "444",
+      title: "title4",
+      content: "content4",
+      time: 444,
+    },
+    {
+      id: "555",
+      title: "title5",
+      content: "content5",
+      time: 555,
+    },
+    {
+      id: "666",
+      title: "title6",
+      content: "content6",
+      time: 666,
+    },
+    {
+      id: "777",
+      title: "title7",
+      content: "content7",
+      time: 777,
+    },
+    {
+      id: "888",
+      title: "title8",
+      content: "content8",
+      time: 888,
+    },
+    {
+      id: "999",
+      title: "title9",
+      content: "content9",
+      time: 999,
+    },
+  ];
 
-    return (
-        <>
-            <Box sx={{
-                border: "1px solid red",
-                display: "flex",
-                flexDirection: "column",
-                gap: "15px"
-            }}>
-                {listData.map((item)=>{
-                    return <BlogCard key={item.id} blog={item}/>
-                })}
-            </Box>
-        </>
-    )
-}
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+          width: "100%",
+        }}
+      >
+        {listData.map((item) => {
+          return <BlogCard key={item.id} blog={item} />;
+        })}
+      </Box>
+    </Box>
+  );
+};
 
 export default Blog;
--- frontEnd/superlion/src/pages/Blog/BlogCard.tsx
+++ frontEnd/superlion/src/pages/Blog/BlogCard.tsx
@@ -1,24 +1,60 @@
-import { Box,Typography } from "@mui/material";
-import { BlogProps } from "src/models/blog"
+import { Box, Divider } from "@mui/material";
+import { BlogProps } from "src/models/blog";
 import { FC } from "react";
 
 interface Props {
-    blog: BlogProps
+  blog: BlogProps;
 }
 
-
-const BlogCard: FC<Props> = ({ blog })=>{
-    return (
-        <Box sx={{
-            width:"100%",
-            height:"300px",
-            border:"1px solid green"
-        }}>
-            <Typography>{blog.title}</Typography>
-            <Typography>{blog.content}</Typography>
-            <Typography>{blog.time}</Typography>
-        </Box>
-    )
-}
+const BlogCard: FC<Props> = ({ blog }) => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        padding: "10px 20px",
+        backgroundColor:"#fff",
+        boxShadow:"0 0 10px rgba(0,0,0,.1)",
+        borderRadius:"5px",
+        boxSizing:"border-box"
+      }}
+    >
+      <Box
+        sx={{
+          height: "60px",
+          display: "flex",
+          justifyContent: "left",
+          alignItems: "center",
+          fontSize:"20px",
+          fontWeight:600
+        }}
+      >
+        {blog.title}
+      </Box>
+      <Divider />
+      <Box
+        sx={{
+          height: "200px",
+          padding:"15px 0"
+        }}
+      >
+        {blog.content}
+      </Box>
+      <Divider />
+      <Box
+        sx={{
+          height: "40px",
+          display: "flex",
+          justifyContent: "right",
+          alignItems: "center",
+          fontSize:"17px",
+          fontWeight:500,
+          color:"#666"
+        }}
+      >
+        {blog.time}
+      </Box>
+    </Box>
+  );
+};
 
 export default BlogCard;
